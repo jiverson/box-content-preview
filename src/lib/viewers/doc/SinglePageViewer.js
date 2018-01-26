a843a3b7fb639a0d087c305049bfa1d187a439b7
jiverson <jiverson222@gmail.com>
2018-01-26 14:18:54 -0700

    New: initial page range

--- src/lib/viewers/doc/SinglePageViewer.js
+++ src/lib/viewers/doc/SinglePageViewer.js
@@ -6,6 +6,43 @@ class SinglePageViewer extends DocumentViewer {
     //--------------------------------------------------------------------------
 
     /**
+     * Load PDF from representation URL and set as document for pdf.js. Cache
+     * the loading task so we can cancel if needed
+     *
+     * @protected
+     * @param {Object} docInitParams - The params to pass to pdfjs
+     * @return {Promise} Promise to initialize Viewer
+     */
+    initLoadingTask(docInitParams) {
+        const { url } = docInitParams;
+
+        const pageRange = this.getViewerOption('pageRange');
+
+        if (typeof pageRange !== 'number') {
+            return super.initLoadingTask(docInitParams);
+        }
+
+        this.pdfLoadingTask = PDFJS.getDocument(docInitParams);
+        return this.pdfLoadingTask.then((doc) => {
+            return doc
+                .getPage(pageRange)
+                .then(() => {
+                    // Set the number of pages to 1 and replace the pages promise to the one you load
+                    Object.defineProperty(doc, 'numPages', { value: 1 });
+                    Object.defineProperty(doc.transport, 'pagePromises', {
+                        value: doc.transport.pagePromises.slice(pageRange - 1)
+                    });
+
+                    this.pdfViewer.setDocument(doc);
+                    this.initLinkService(doc, url);
+                })
+                .catch((err) => {
+                    this.triggerError(err);
+                });
+        });
+    }
+
+    /**
      * Get pdf.js viewer.
      *
      * @protected
